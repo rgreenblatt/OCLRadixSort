dfb89e898d2027977b1e8d54796a321aa9cde9f4
rgreenblatt <greenblattryan@gmail.com>
2019-06-17 16:52:30 -0400

    WIP, moving to boost compute

--- example/Trivial.cpp
+++ example/Trivial.cpp
@@ -1,36 +1,24 @@
-#include "Common.hpp"
+#include "../include/OCLRadixSort.hpp"
 
 int main() {
+  // create instance for device
+  bc::device device = bc::system::default_device();
+  bc::context context = bc::context(device);
+  bc::command_queue command_queue = bc::command_queue(context, device);
 
-    // create instance for device
-    SimpleRadixSort64bit rs { getDevice() };
+  RadixSort<> rs(context, command_queue);
 
-    std::vector<SimpleRadixSort64bit::KeyType > v ( 1 << 25 );
-    std::cout << "generating " << v.size() << " keys...." << std::endl;
+  std::vector<unsigned> keys_host(1 << 25);
+  std::cout << "generating " << keys_host.size() << " keys_host...." << std::endl;
+  std::generate(keys_host.begin(), keys_host.end(), rand);
 
-    auto rng = makeRng( rs.maxValue() );
-    std::generate( v.begin(), v.end(), rng );
+  bc::vector<unsigned> keys(keys_host.begin(), keys_host.end(), command_queue);
+  bc::vector<double> values(keys_host.size(), context);
 
-    // take a copy of v for permutation check
-    std::vector<SimpleRadixSort64bit::KeyType > vCopy = v;
+  rs.sort(keys, values);
 
-    try {
-        // sort method is STL-style
-        rs.sort( v.begin(), v.end() );
-    } catch (const cl::Error& e) {
-        std::cerr << "CL Error: " << e.err() << " " << e.what() << std::endl;
-        return 1;
-    }
+  bc::copy(keys.begin(), keys.end(), keys_host.begin(), command_queue);
 
-    std::cout << "Sorted: " << std::boolalpha << std::is_sorted( v.begin(), v.end() ) << std::endl;
-    std::cout << "Permutation correct: " << std::boolalpha << checkPermutation(v, vCopy, rs.permutation()) << std::endl;
-
-    const auto pi = rs.profilingInfo();
-    std::cout << "Profiling: " << std::endl;
-//    std::cout << "transpose:\t" << pi.transpose << "ns\n";
-//    std::cout << "histogram:\t" << pi.histogram << "ns\n";
-//    std::cout << "scan:\t\t" << pi.scan << "ns\n";
-//    std::cout << "reorder:\t" << pi.reorder << "ns" << std::endl;
-
-    std::cout << "total kernel execution time: " << pi.total() * 1e-6 << "ms" << std::endl;
+  std::cout << "Sorted: " << std::boolalpha
+            << std::is_sorted(keys_host.begin(), keys_host.end()) << std::endl;
 }
